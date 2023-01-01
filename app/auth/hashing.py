358d216dc7e61c8edad534ff402f090a7d76e545
Prakhar <forestla319@gmail.com>
2023-01-01 19:38:26 +0530

    added few more comments

--- app/auth/hashing.py
+++ app/auth/hashing.py
@@ -3,8 +3,10 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
+# to verify any plain password with hashed version of it.
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
+# to get hashed version of any password.
 def get_password_hash(password):
     return pwd_context.hash(password)
